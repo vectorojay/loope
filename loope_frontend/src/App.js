035220318360ab331bef90c893bb98cdaa06dd73
vectordee <vectorojay@gmail.com>
2024-11-02 16:14:59 +0100

    create components, setup routes, install frontend dependencies

--- loope_frontend/src/App.js
+++ loope_frontend/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useNavigate, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./container/Home";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
