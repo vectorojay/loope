a9669efa20c9b23ca5cb6f893c1eda874c609ebb
vectordee <vectorojay@gmail.com>
2024-11-04 17:02:41 +0100

    implement google oauth functionality, create .env for id

--- loope_frontend/src/components/Login.jsx
+++ loope_frontend/src/components/Login.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import loopeVideo from "../assets/loopevid.mp4";
+import logo from "../assets/loopelogo.png";
+import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
 
 const Login = () => {
-  return <div>Login</div>;
+  const login = useGoogleLogin({
+    onSuccess: (response) => {
+      console.log(response);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  return (
+    <div className="flex flex-col justify-start items-center h-screen">
+      <div className="relative w-full h-full">
+        <video
+          className="w-full h-full object-cover"
+          src={loopeVideo}
+          type="video/mp4"
+          autoPlay
+          loop
+          controls={false}
+          muted
+        ></video>
+        <div className="absolute flex flex-col justify-center items-end top-0 right-0 bottom-0 left-0 bg-blackOverlay">
+          {/* <img src={logo} /> */}
+          <p className="text-gray-100 text-8xl font-sans mr-80">
+            lo
+            <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-yellow-500 relative inline-block rounded-full">
+              <span className="relative text-white">o</span>
+            </span>
+            pe
+          </p>
+          <span className="mr-80 text-yellow-500 mt-4">
+            Create. Share. Loope
+          </span>
+          <div className="shadow-2xl">
+            {/* <GoogleLogin
+              style={{
+                backgroundColor: "#4285f4",
+                color: "white",
+                border: "none",
+                borderRadius: "5px",
+                padding: "10px 20px",
+                fontSize: "16px",
+                cursor: "pointer",
+                width: "10px !important",
+              }}
+              onSuccess={successMessage}
+              onError={errorMessage}
+            /> */}
+            <button
+              onClick={() => login()}
+              className="flex justify-center items-center p-3 mr-80 mt-6 bg-mainColor rounded-lg cursor-pointer outline-none"
+              type="button"
+            >
+              <FcGoogle className="mr-4" /> Sign in with Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- loope_frontend/src/index.js
+++ loope_frontend/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const env = process.env.REACT_APP_GOOGLE_API_TOKEN;
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <GoogleOAuthProvider clientId={env}>
+        <App />
+      </GoogleOAuthProvider>
     </Router>
   </React.StrictMode>
 );
